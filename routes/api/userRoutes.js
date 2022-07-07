e8088dbaa285dbb91fea918292653c7733c7bd0b
Lunirs <dhong0925@gmail.com>
2022-07-07 16:15:55 -0400

    Added user Routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getOneUser,
+  newUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/userController");
+
+// route for /api/users
+router.route("/").get(getAllUsers).post(newUser);
+
+//route for /api/users/:userId
+router.route("/:userId").get(getOneUser).put(updateUser).delete(deleteUser);
+
+// route for /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
