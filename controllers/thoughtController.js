c684c9a3b8fcfbec28c45e6ad359ef169d5cf163
Lunirs <dhong0925@gmail.com>
2022-07-07 16:46:27 -0400

    Added controller for finding all thoughts and finding one thought

---
+++ controllers/thoughtController.js
@@ -0,0 +1,41 @@
+const { Thought, User } = require("../models");
+
+const thoughtController = {
+  // GET all thoughts
+  async getAllThoughts(req, res) {
+    try {
+      const thoughtData = await Thought.find().select("-__v");
+
+      res.status(200).json(thoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  // GET one Thought by its id
+  async getOneThought(req, res) {
+    try {
+      const thoughtData = await Thought.findOne({
+        _id: req.params.thoughtId,
+      }).select("-__v");
+
+      if (!thoughtData) {
+        return res
+          .status(404)
+          .json({ message: "This thought does not exist." });
+      }
+
+      res.status(200).json(thoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  // CREATE a thought
+  // UPDATE a thought by its id
+  // DELETE a thought by its id
+  // CREATE a reaction to a thought by its id
+  // DELETE a reaction from a thought by its id
+};
+
+module.exports = thoughtController;
